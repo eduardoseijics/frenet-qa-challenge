19f37f6af1a97e8f55a06ef8e5a5a78d6845828e
Eduardo Seiji <eduardoseiji202@gmail.com>
2026-06-28 19:04:02 -0300

    refactor: remove console.log

--- tests/register/happy-path.spec.ts
+++ tests/register/happy-path.spec.ts
@@ -12,14 +12,10 @@ test.describe('New User Registration Flow - Frenet', () => {
   test('Should successfully register a new user', async ({ registerPage, page }) => {
     test.fixme(true, 'reCAPTCHA cannot be resolved automatically in a headless/automated environment.');
 
-    console.log(`Starting registration test for email: ${userData.email}`);
-
     await registerPage.fillForm(userData);
 
-    console.log('Attempting to solve the reCAPTCHA...');
     await registerPage.clickRecaptcha();
 
-    console.log('Waiting for reCAPTCHA to be solved to enable the registration button...');
     await expect(registerPage.submitButton).toBeEnabled({ timeout: 60000 });
 
     await registerPage.submit();
